a62a1098fbdd55b1a1a969d0baf14fc2a736a2a6
yutat23 <yutat23@gmail.com>
2023-03-22 03:15:26 +0900

    first commit

---
+++ battlecats-gatya.py
@@ -0,0 +1,74 @@
+import requests
+from datetime import datetime, timedelta
+
+def curl_command_to_request(curl_command: str):
+    parts = curl_command.split()
+    url = ""
+    headers = {}
+    method = "GET"
+    for i, part in enumerate(parts):
+        if part == "-X":
+            method = parts[i + 1]
+        elif part == "-H":
+            header = parts[i + 1].split(":")
+            headers[header[0]] = header[1].strip()
+        elif part.startswith("http"):
+            url = part
+
+    return method, url, headers
+
+def is_number(s: str) -> bool:
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def parse_tsv(tsv):
+    lines = tsv.split("\n")
+    now = datetime.now()
+    last_date_object = None
+    for line in lines:
+        if line.startswith("[start]"):
+          continue
+        if line.startswith("[end]"):
+            break
+        cells = line.split("\t")
+        date = cells[0][:4] + '-' + cells[0][4:6] + '-' + cells[0][6:8]
+
+        for col in  range(1, len(cells)):
+            if not is_number(cells[-col]):
+                title = cells[-col]
+                break
+        date_object = datetime.strptime(date, "%Y-%m-%d") + timedelta(hours=11)
+        if date_object >= now:
+            if last_date_object == None:
+              last_date_object = date_object
+            
+            # now gatya
+            if last_date_object == date_object:
+              print(f"\033[38;5;210m{date} {title}\033[0m")
+
+            # future gatya
+            else:
+              print(f"\033[38;5;216m{date} {title}\033[0m")
+        else:
+            print(f"\033[38;5;245m{date} {title}\033[0m")
+
+def main():
+    command_url = "https://bc-seek.godfat.org/seek/jp/gatya.tsv"
+    headers = {
+        'referer' : 'https://bc.godfat.org/',
+        }
+    response = requests.get(command_url, headers=headers)
+
+    if response.status_code == 200:
+        tsv = response.text
+        parse_tsv(tsv)
+
+    else:
+        print(f"failed request: {response.status_code}")
+
+if __name__ == "__main__":
+    main()
+
